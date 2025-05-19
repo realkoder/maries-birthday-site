6874e66fe8c201b37f3dde99dcc1a0abe3c813df
realkoder <alexanderbtcc@gmail.com>
2025-05-20 01:35:19 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+        <header className="flex flex-col items-center gap-9">
+          <p>Tillykke MISKATH!</p>
+        </header>
+        <p>🇩🇰 Så blev det din fødselsdag - YAY 🇩🇰</p>
+      </div>
+    </main>
+  );
+}
+
+export default App;
